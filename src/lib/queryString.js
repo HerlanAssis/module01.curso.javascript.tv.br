5515a8ff34428e32dfb7ee303e301f65a453dd87
Herlan Assis Pereira da Silva <Herlanassis@gmail.com>
2022-09-15 19:34:25 +0000

    feat: add querystring lib and tests

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -11,3 +11,19 @@ module.exports.queryString = (str) => {
 
   return Object.entries(str).map(keyValueToString).join("&");
 };
+
+module.exports.parse = (string) => {
+  const arrayOfParams = string.split("&");
+  const obj = Object.fromEntries(
+    arrayOfParams.map((item) => {
+      const [key, value] = item.split("=");
+
+      if (value.includes(",")) {
+        return [key, value.split(",")];
+      }
+
+      return [key, value];
+    })
+  );
+  return obj;
+};
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,4 +1,4 @@
-const { queryString } = require("./queryString");
+const { queryString, parse } = require("./queryString");
 
 describe("Object to query string", () => {
   it("should create a valid query string when an object is provided", () => {
@@ -37,5 +37,43 @@ describe("Object to query string", () => {
     expect(() => {
       queryString(obj);
     }).toThrowError();
+  });
+});
+
+describe("Querystring to Object", () => {
+  it("should convert a querystring to object", () => {
+    const qs = "name=Herlan&profession=developer";
+
+    const result = parse(qs);
+    const expected = {
+      name: "Herlan",
+      profession: "developer",
+    };
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should convert a querystring of a single key-value object", () => {
+    const qs = "name=Herlan";
+
+    const expected = {
+      name: "Herlan",
+    };
+    const result = parse(qs);
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should conver a querystring to an objeto take care of comma separated values", () => {
+    const qs = "name=herlan&profession=developer&abilities=React,Django";
+
+    const expected = {
+      name: "herlan",
+      profession: "developer",
+      abilities: ["React", "Django"],
+    };
+    const result = parse(qs);
+
+    expect(result).toEqual(expected);
   });
 });
